7b8cacf6ce1925c16d461e7938c26b04ac071277
Y praveen <84093322+praveen369-369@users.noreply.github.com>
2026-07-15 11:39:57 +0530

    Add files via upload

---
+++ number of rows.py
@@ -0,0 +1,9 @@
+n = 4
+i = 1
+while i <= n:
+    j = 1
+    while j <= i:
+        print(j, end=" ")
+        j += 1
+    print()
+    i += 1
---
+++ row.py
@@ -0,0 +1,10 @@
+i=1
+while i<=5:
+    j=1
+    while j<=5:
+        # print(f"{i}x{j}={i*j}")
+        print(i,j)
+
+        j=j+1
+    print()
+    i+=1
